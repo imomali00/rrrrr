f2c06869c73e81d1dede1d6a374ce11c7687596f
Macbook Air <macbookair@MacBook-Air-Macbook-2.local>
2023-03-26 11:01:21 +0500

    aaaaa

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -41,7 +41,6 @@ const Form = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(inputFields);
   };
 
   return (
@@ -52,77 +51,64 @@ const Form = () => {
         </div>
 
         <form onSubmit={handleSubmit}>
-          {inputFields.map((input, fieldIndex) => {
-            return (
-              <div key={fieldIndex}>
-                <div className="mx-20 mt-6">
-                  <p>Option name</p>
-                  <div className="flex items-center">
+          {inputFields.map((input, fieldIndex) => (
+            <div key={fieldIndex}>
+              <div className="mx-20 mt-6">
+                <p>Option name</p>
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={input.name}
+                    onChange={(event) => handleFormChange(fieldIndex, event)}
+                    autoComplete="off"
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 w-[500px]"
+                  />
+                  <button onClick={() => removeFields(fieldIndex)}>
+                    <i className="fa-solid fa-trash-can ml-3 cursor-pointer"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="mx-20 mt-6">
+                <p>Option values</p>
+                {input.values.map((value, valueIndex) => (
+                  <div className="flex items-center" key={valueIndex}>
                     <input
                       type="text"
-                      name="name"
-                      id="name"
-                      value={input.name}
-                      onChange={(event) => handleFormChange(fieldIndex, event)}
+                      name="value"
+                      id="value"
+                      value={value}
+                      onChange={(event) =>
+                        handleValueChange(fieldIndex, valueIndex, event)
+                      }
                       autoComplete="off"
                       className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 w-[500px]"
                     />
-                    <button onClick={() => removeFields(fieldIndex)}>
+                    <button
+                      onClick={() => handleDeleteValue(fieldIndex, valueIndex)}
+                    >
                       <i className="fa-solid fa-trash-can ml-3 cursor-pointer"></i>
                     </button>
                   </div>
+                ))}
+                <div className="flex items-center">
+                  <input
+                    type="text"
+                    placeholder="Add another value"
+                    aria-disabled
+                    onClick={() => handleAddValue(fieldIndex)}
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6 px-2 w-[500px]"
+                  />
+                  <div className="ml-6">{""}</div>
                 </div>
-                <div className="mx-20 mt-6">
-                  <p>Option values</p>
-                  {input.values.map((value, valueIndex) => {
-                    return (
-                      <div className="flex items-center" key={valueIndex}>
-                        <input
-                          type="text"
-                          name="value"
-                          id="value"
-                          value={value}
-                          onChange={(event) =>
-                            handleValueChange(fieldIndex, valueIndex, event)
-                          }
-                          autoComplete="off"
-                          className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 w-[500px]"
-                        />
-                        <button
-                          onClick={() =>
-                            handleDeleteValue(fieldIndex, valueIndex)
-                          }
-                        >
-                          <i className="fa-solid fa-trash-can ml-3 cursor-pointer"></i>
-                        </button>
-                      </div>
-                    );
-                  })}
-                  <div className="flex items-center">
-                    <button
-                      className="block w-full"
-                      onClick={() => handleAddValue(fieldIndex)}
-                    >
-                      <input
-                        type="text"
-                        placeholder="Add another value"
-                        disabled
-                        className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2 w-[500px]"
-                      />
-                    </button>
-                    <div className="ml-6">{""}</div>
-                  </div>
-                  <button
-                    className="mt-5 bg-transparent text-black-700 font-semibold py-2 px-4 border border-black-900"
-                    type="submit"
-                  >
-                    Done
-                  </button>
-                </div>
-                <hr className="mt-3" />
+                <button className="mt-5 bg-transparent text-black-700 font-semibold py-2 px-4 border border-black-900">
+                  Done
+                </button>
               </div>
-            );
-          })}
+              <hr className="mt-3" />
+            </div>
+          ))}
         </form>
 
         <div className="flex h-[100px] items-center text-blue-500">
@@ -132,36 +118,17 @@ const Form = () => {
           </button>
         </div>
         <hr />
-        <table className="table-auto ml-10 h-10">
-          <thead>
-            <tr>
-              <th>Values</th>
-            </tr>
-          </thead>
+        <table className="table-auto ml-[50px] h-auto">
+          <h1>Variants</h1>
           {inputFields &&
             inputFields.map((s, i) => {
               return (
                 <tbody key={i}>
-                  <tr>{s.values && <td>{s.values}</td>}</tr>
+                  <tr className="h-12 ">{s.values && <td>{s.values}</td>}</tr>
                 </tbody>
               );
             })}
         </table>
-        {/* {inputFields &&
-          inputFields.map((s, i) => {
-            return (
-              <table className="table-auto ml-10 h-10" key={i}>
-                <thead>
-                  <tr>
-                    <th>Values</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>{s.values && <td>{s.values}</td>}</tr>
-                </tbody>
-              </table>
-            );
-          })} */}
       </div>
     </div>
   );
